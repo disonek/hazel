dcc58a6bb40639fdc8d8bb6922432dc40c6ec3fe
disonek <disonek@gmail.com>
2020-10-08 16:07:12 +0200

    (Renderer2D: added draw call splitting and stats)

--- hazel/src/hazel/renderer/Renderer2D.cpp
+++ hazel/src/hazel/renderer/Renderer2D.cpp
@@ -20,9 +20,9 @@ struct QuadVertex
 
 struct Renderer2DData
 {
-    const uint32_t MaxQuads = 10000;
-    const uint32_t MaxVertices = MaxQuads * 4;
-    const uint32_t MaxIndices = MaxQuads * 6;
+    static const uint32_t MaxQuads = 20000;
+    static const uint32_t MaxVertices = MaxQuads * 4;
+    static const uint32_t MaxIndices = MaxQuads * 6;
     static const uint32_t MaxTextureSlots = 32; // TODO: RenderCaps
 
     Ref<VertexArray> QuadVertexArray;
@@ -38,6 +38,7 @@ struct Renderer2DData
     uint32_t TextureSlotIndex = 1; // 0 = white texture
 
     std::array<glm::vec4, 4> QuadVertexPositions;
+    Renderer2D::Statistics Stats;
 };
 
 static Renderer2DData s_Data;
@@ -138,6 +139,17 @@ void Renderer2D::Flush()
         s_Data.TextureSlots[i]->Bind(i);
 
     RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+    s_Data.Stats.DrawCalls++;
+}
+
+void Renderer2D::FlushAndReset()
+{
+    EndScene();
+
+    s_Data.QuadIndexCount = 0;
+    s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+    s_Data.TextureSlotIndex = 1;
 }
 
 void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, const glm::vec4& color)
@@ -148,6 +160,9 @@ void Renderer2D::DrawQuad(const glm::vec2& position, const glm::vec2& size, cons
 void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, const glm::vec4& color)
 {
     HZ_PROFILE_FUNCTION();
+
+    if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        FlushAndReset();
 
     const float textureIndex = 0.0f; // White Texture
     const float tilingFactor = 1.0f;
@@ -184,6 +199,8 @@ void Renderer2D::DrawQuad(const glm::vec3& position, const glm::vec2& size, cons
     s_Data.QuadVertexBufferPtr++;
 
     s_Data.QuadIndexCount += 6;
+
+    s_Data.Stats.QuadCount++;
 }
 
 void Renderer2D::DrawQuad(const glm::vec2& position,
@@ -202,6 +219,10 @@ void Renderer2D::DrawQuad(const glm::vec3& position,
                           const glm::vec4& tintColor)
 {
     HZ_PROFILE_FUNCTION();
+
+    if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        FlushAndReset();
+
     constexpr glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
 
     float textureIndex = 0.0f;
@@ -252,6 +273,8 @@ void Renderer2D::DrawQuad(const glm::vec3& position,
     s_Data.QuadVertexBufferPtr++;
 
     s_Data.QuadIndexCount += 6;
+
+    s_Data.Stats.QuadCount++;
 }
 
 void Renderer2D::DrawRotatedQuad(const glm::vec2& position,
@@ -268,6 +291,9 @@ void Renderer2D::DrawRotatedQuad(const glm::vec3& position,
                                  const glm::vec4& color)
 {
     HZ_PROFILE_FUNCTION();
+
+    if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        FlushAndReset();
 
     const float textureIndex = 0.0f; // White Texture
     const float tilingFactor = 1.0f;
@@ -304,6 +330,8 @@ void Renderer2D::DrawRotatedQuad(const glm::vec3& position,
     s_Data.QuadVertexBufferPtr++;
 
     s_Data.QuadIndexCount += 6;
+
+    s_Data.Stats.QuadCount++;
 }
 
 void Renderer2D::DrawRotatedQuad(const glm::vec2& position,
@@ -325,6 +353,9 @@ void Renderer2D::DrawRotatedQuad(const glm::vec3& position,
 {
     HZ_PROFILE_FUNCTION();
 
+    if(s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+        FlushAndReset();
+
     constexpr glm::vec4 color = {1.0f, 1.0f, 1.0f, 1.0f};
 
     float textureIndex = 0.0f;
@@ -376,6 +407,18 @@ void Renderer2D::DrawRotatedQuad(const glm::vec3& position,
     s_Data.QuadVertexBufferPtr++;
 
     s_Data.QuadIndexCount += 6;
+
+    s_Data.Stats.QuadCount++;
+}
+
+void Renderer2D::ResetStats()
+{
+    memset(&s_Data.Stats, 0, sizeof(Statistics));
+}
+
+Renderer2D::Statistics Renderer2D::GetStats()
+{
+    return s_Data.Stats;
 }
 
 } // namespace hazel
--- sandbox/src/Sandbox2D.cpp
+++ sandbox/src/Sandbox2D.cpp
@@ -8,7 +8,8 @@
 
 Sandbox2D::Sandbox2D()
     : Layer("Sandbox2D")
-    , m_CameraController(1280.0f / 720.0f, true)
+    , m_CameraController{1280.0f / 720.0f, true}
+    , m_SquareColor{0.2f, 0.3f, 0.8f, 1.0f}
 {
 }
 
@@ -32,6 +33,7 @@ void Sandbox2D::OnUpdate(hazel::Timestep ts)
     m_CameraController.OnUpdate(ts);
 
     // Render
+    hazel::Renderer2D::ResetStats();
     {
         HZ_PROFILE_SCOPE("Renderer Prep");
         hazel::RenderCommand::SetClearColor({0.1f, 0.1f, 0.1f, 1});
@@ -46,9 +48,20 @@ void Sandbox2D::OnUpdate(hazel::Timestep ts)
         hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
         hazel::Renderer2D::DrawRotatedQuad({1.0f, 0.0f}, {0.8f, 0.8f}, -45.0f, {0.8f, 0.2f, 0.3f, 1.0f});
         hazel::Renderer2D::DrawQuad({-1.0f, 0.0f}, {0.8f, 0.8f}, {0.8f, 0.2f, 0.3f, 1.0f});
-        hazel::Renderer2D::DrawQuad({0.5f, -0.5f}, {2.5f, 0.75f}, {0.2f, 0.3f, 0.8f, 1.0f});
-        hazel::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {10.0f, 10.0f}, m_CheckerboardTexture, 10.0f);
+        hazel::Renderer2D::DrawQuad({0.5f, -0.5f}, {2.5f, 0.75f}, m_SquareColor);
+        hazel::Renderer2D::DrawQuad({0.0f, 0.0f, -0.1f}, {20.0f, 20.0f}, m_CheckerboardTexture, 10.0f);
         hazel::Renderer2D::DrawRotatedQuad({-2.0f, 0.0f, 0.0f}, {1.0f, 1.0f}, rotation, m_CheckerboardTexture, 20.0f);
+        hazel::Renderer2D::EndScene();
+
+        hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+        for(float y = -5.0f; y < 5.0f; y += 0.5f)
+        {
+            for(float x = -5.0f; x < 5.0f; x += 0.5f)
+            {
+                glm::vec4 color = {(x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f};
+                hazel::Renderer2D::DrawQuad({x, y}, {0.45f, 0.45f}, color);
+            }
+        }
         hazel::Renderer2D::EndScene();
     }
 }
@@ -58,6 +71,13 @@ void Sandbox2D::OnImGuiRender()
     HZ_PROFILE_FUNCTION();
 
     ImGui::Begin("Settings");
+
+    auto stats = hazel::Renderer2D::GetStats();
+    ImGui::Text("Renderer2D Stats:");
+    ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+    ImGui::Text("Quads: %d", stats.QuadCount);
+    ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+    ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
     ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
     ImGui::End();
